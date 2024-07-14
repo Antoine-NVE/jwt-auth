f39e12e64d38a06e6e9e57bd21c6970d3ff77f40
Antoine-NVE <antoinenavette@gmail.com>
2024-07-14 11:02:28 +0200

    Mise en place du main côté serveur

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+mongoose
+    .connect(process.env.MONGODB)
+    .then(() => console.log('Connexion à MongoDB réussie'))
+    .catch((error) => console.error(`Connexion à MongoDB échouée : ${error}`));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+const port = 3000;
+app.listen(port, () => console.log(`Le serveur tourne sur le port ${port}`));
