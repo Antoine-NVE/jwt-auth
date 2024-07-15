c66cb1173916bd2ca236b4937a7d5ba5e978cd93
Antoine-NVE <antoinenavette@gmail.com>
2024-07-15 14:49:43 +0200

    Création du middleware pour vérifier la connexion

---
+++ server/middlewares/auth.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    try {
+        // On vérifie le JWT
+        const decodedToken = jwt.verify(req.cookies.jwt, process.env.SECRET);
+
+        // On rend accessible l'id aux fonctions suivantes
+        req.auth = {
+            id: decodedToken.id,
+        };
+
+        next();
+    } catch (error) {
+        res.status(401).json(error);
+    }
+};
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../controllers/user.controller');
+const auth = require('../middlewares/auth');
 
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.get('/logout', userController.logout);
+router.get('/logout', auth, userController.logout);
 
 // router.get('/set-cookie', userController.setCookie);
 // router.get('/get-cookie', userController.getCookie);
