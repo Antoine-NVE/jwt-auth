2c6b0595047d35d0d91bdd822c19152214be0509
Antoine-NVE <antoinenavette@gmail.com>
2024-07-16 11:42:05 +0200

    Création d'un système de refresh du JWT de connexion

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,9 +1,23 @@
 const jwt = require('jsonwebtoken');
 
+const authService = require('../services/auth.service');
+
 module.exports = (req, res, next) => {
     try {
         // On vérifie le JWT
         const decodedToken = jwt.verify(req.cookies.jwt, process.env.SECRET);
+
+        // On récupère l'heure à laquelle le JWT aura vécu plus de 50% de son temps
+        const halfLifeTime = (decodedToken.iat + decodedToken.exp) / 2;
+
+        // On récupère l'heure actuelle
+        const currentTime = Date.now() / 1000;
+
+        // Si l'on a dépassé la moitié de la vie du token, on le refresh
+        // Si l'utilisateur ne s'est pas connecté depuis trop longtemps, le token expire et le refresh n'est plus possible, il faut se login
+        if (currentTime > halfLifeTime) {
+            authService.connectUser(res, decodedToken.id);
+        }
 
         // On rend accessible l'id aux fonctions suivantes
         req.auth = {
