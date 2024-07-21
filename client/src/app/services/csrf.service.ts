4c20cec3b2624463cb5981006195ecc0e2b6b4eb
Antoine-NVE <antoinenavette@gmail.com>
2024-07-21 11:10:05 +0200

    Création du service pour récupérer le token CSRF

---
+++ client/src/app/services/csrf.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class CsrfService {
+    private readonly API = 'http://localhost:3000/api/csrf';
+
+    constructor(private http: HttpClient) {}
+
+    public read(): Observable<{ csrfToken: string }> {
+        return this.http.get<{ csrfToken: string }>(this.API);
+    }
+}
