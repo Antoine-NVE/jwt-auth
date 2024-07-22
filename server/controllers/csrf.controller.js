4af58f99a58b6db61d256758ddd4a78fae6fab21
Antoine-NVE <antoinenavette@gmail.com>
2024-07-22 15:12:55 +0200

    Déplacement de l'appel de la fonction de création du token CSRF pour éviter d'en créer un inutilement

--- server/controllers/csrf.controller.js
+++ server/controllers/csrf.controller.js
@@ -1,3 +1,10 @@
+const csrfService = require('../services/csrf.service');
+
 exports.read = (req, res) => {
+    // À chaque requête, on vérifie qu'on a bien un token CSRF
+    if (!req.cookies.csrfToken) {
+        csrfService.createToken(req, res);
+    }
+
     res.status(200).json({ csrfToken: req.cookies.csrfToken });
 };
--- server/index.js
+++ server/index.js
@@ -21,11 +21,6 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 
-    // À chaque requête, on vérifie qu'on a bien un token CSRF
-    if (!req.cookies.csrfToken) {
-        csrfService.createToken(req, res);
-    }
-
     next();
 });
 
